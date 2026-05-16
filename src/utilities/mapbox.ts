03d1dd638bae2c398edb72d6a930d1a4bc860d83
Everett Blakley <39717736+everettblakley@users.noreply.github.com>
2026-05-16 15:36:16 -0600

    chore: add env types

--- src/utilities/mapbox.ts
+++ src/utilities/mapbox.ts
@@ -2,6 +2,6 @@ import mapboxgl from "mapbox-gl";
 import mapboxSdk from "@mapbox/mapbox-sdk";
 import geocoding from "@mapbox/mapbox-sdk/services/geocoding";
 
-mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN as string;
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
 export { mapboxgl as mapbox, mapboxSdk, geocoding };
